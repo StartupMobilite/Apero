3288009abb269278ba1e5a6b45c1d52b6361c985
namda32 <namda32@gmail.com>
2015-12-02 16:46:46 +0100

    TransfertSolution

---
+++ Rapports/menutest/DrawerLayout_V7_Tutorial/DrawerLayout_V7_Tutorial/MainActivity.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using System.Collections.Generic;
+
+namespace DrawerLayout_V7_Tutorial
+{
+	[Activity (Label = "DrawerLayout_V7_Tutorial", MainLauncher = true, Icon = "@drawable/icon", Theme="@style/MyTheme")]
+	public class MainActivity : ActionBarActivity
+	{
+		private SupportToolbar mToolbar;
+		private MyActionBarDrawerToggle mDrawerToggle;
+		private DrawerLayout mDrawerLayout;
+		private ListView mLeftDrawer;
+		private ListView mRightDrawer;
+		private ArrayAdapter mLeftAdapter;
+		private ArrayAdapter mRightAdapter;
+		private List<string> mLeftDataSet;
+		private List<string> mRightDataSet;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+	
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			mToolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+			mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+			mLeftDrawer = FindViewById<ListView>(Resource.Id.left_drawer);
+			mRightDrawer = FindViewById<ListView>(Resource.Id.right_drawer);
+
+			mLeftDrawer.Tag = 0;
+			mRightDrawer.Tag = 1;
+
+			SetSupportActionBar(mToolbar);
+		
+			mLeftDataSet = new List<string>();
+			mLeftDataSet.Add ("Left Item 1");
+			mLeftDataSet.Add ("Left Item 2");
+			mLeftAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mLeftDataSet);
+			mLeftDrawer.Adapter = mLeftAdapter;
+
+			mRightDataSet = new List<string>();
+			mRightDataSet.Add ("Right Item 1");
+			mRightDataSet.Add ("Right Item 2");
+			mRightAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mRightDataSet);
+			mRightDrawer.Adapter = mRightAdapter;
+
+			mDrawerToggle = new MyActionBarDrawerToggle(
+				this,							//Host Activity
+				mDrawerLayout,					//DrawerLayout
+				Resource.String.openDrawer,		//Opened Message
+				Resource.String.closeDrawer		//Closed Message
+			);
+
+			mDrawerLayout.SetDrawerListener(mDrawerToggle);
+			SupportActionBar.SetHomeButtonEnabled(true);
+			SupportActionBar.SetDisplayShowTitleEnabled(true);
+			mDrawerToggle.SyncState();
+
+			if (bundle != null)
+			{
+				if (bundle.GetString("DrawerState") == "Opened")
+				{
+					SupportActionBar.SetTitle(Resource.String.openDrawer);
+				}
+
+				else
+				{
+					SupportActionBar.SetTitle(Resource.String.closeDrawer);
+				}
+			}
+
+			else
+			{
+				//This is the first the time the activity is ran
+				SupportActionBar.SetTitle(Resource.String.closeDrawer);
+			}
+		}
+			
+		public override bool OnOptionsItemSelected (IMenuItem item)
+		{		
+			switch (item.ItemId)
+			{
+
+			case Android.Resource.Id.Home:
+				//The hamburger icon was clicked which means the drawer toggle will handle the event
+				//all we need to do is ensure the right drawer is closed so the don't overlap
+				mDrawerLayout.CloseDrawer (mRightDrawer);
+				mDrawerToggle.OnOptionsItemSelected(item);
+				return true;
+
+			case Resource.Id.action_refresh:
+				//Refresh
+				return true;
+
+			case Resource.Id.action_help:
+				if (mDrawerLayout.IsDrawerOpen(mRightDrawer))
+				{
+					//Right Drawer is already open, close it
+					mDrawerLayout.CloseDrawer(mRightDrawer);
+				}
+
+				else
+				{
+					//Right Drawer is closed, open it and just in case close left drawer
+					mDrawerLayout.OpenDrawer (mRightDrawer);
+					mDrawerLayout.CloseDrawer (mLeftDrawer);
+				}
+
+				return true;
+
+			default:
+				return base.OnOptionsItemSelected (item);
+			}
+		}
+			
+		public override bool OnCreateOptionsMenu (IMenu menu)
+		{
+			MenuInflater.Inflate (Resource.Menu.action_menu, menu);
+			return base.OnCreateOptionsMenu (menu);
+		}
+
+		protected override void OnSaveInstanceState (Bundle outState)
+		{
+			if (mDrawerLayout.IsDrawerOpen((int)GravityFlags.Left))
+			{
+				outState.PutString("DrawerState", "Opened");
+			}
+
+			else
+			{
+				outState.PutString("DrawerState", "Closed");
+			}
+
+			base.OnSaveInstanceState (outState);
+		}
+
+		protected override void OnPostCreate (Bundle savedInstanceState)
+		{
+			base.OnPostCreate (savedInstanceState);
+			mDrawerToggle.SyncState();
+		}
+
+		public override void OnConfigurationChanged (Android.Content.Res.Configuration newConfig)
+		{
+			base.OnConfigurationChanged (newConfig);
+			mDrawerToggle.OnConfigurationChanged(newConfig);
+		}
+	}
+}
+
+
