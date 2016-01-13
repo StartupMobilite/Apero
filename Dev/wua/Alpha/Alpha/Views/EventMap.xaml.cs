4ca04fb8620624d127ec4e89170327379b7e1655
namda32 <namda32@gmail.com>
2016-01-13 11:21:59 +0100

    Map control added properties

--- Dev/wua/Alpha/Alpha/Views/EventMap.xaml.cs
+++ Dev/wua/Alpha/Alpha/Views/EventMap.xaml.cs
@@ -12,19 +12,44 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// Pour plus d'informations sur le modèle d'élément Page vierge, voir la page http://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Controls.Maps;
+using Windows.Devices.Geolocation;
 
 namespace Alpha.Views
-{
-    /// <summary>
-    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
-    /// </summary>
+{ 
+
     public sealed partial class EventMap : Page
     {
+        private Geopoint cityCenter;
+
         public EventMap()
         {
             this.InitializeComponent();
+
+            // Add the MapControl and the specify maps authentication key.
+            MapControl MapControl1 = new MapControl();
+            MapControl1.ZoomInteractionMode = MapInteractionMode.GestureAndControl;
+            MapControl1.TiltInteractionMode = MapInteractionMode.GestureAndControl;
+            MapControl1.MapServiceToken = "NU84AueM7GMiPBOwHSoE~Ys6Syu5oct7d2PaDIdzl8g~AsQyIrLBonF8di9BqLCV0kg0tZnAFpbav2HztwHXPdUa3JOgL56tMLZP4E2vlTg8";
+            pageGrid.Children.Add(MapControl1);
+            MapControl1.Center = cityCenter;
+            MapControl1.ZoomLevel = 12;
+            MapControl1.LandmarksVisible = true;
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Specify a known location.
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
+            Geopoint cityCenter = new Geopoint(cityPosition);
+
+            // Set the map location.
+            MapControl1.Center = cityCenter;
+            MapControl1.ZoomLevel = 12;
+            MapControl1.LandmarksVisible = true;
+        }
+
     }
+
+
 }
