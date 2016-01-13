0be000d5a636cc10c5307ff59fa001c5fb462b20
namda32 <namda32@gmail.com>
2016-01-13 12:22:14 +0100

    display map alpha

--- Dev/wua/Alpha/Alpha/Views/EventMap.xaml.cs
+++ Dev/wua/Alpha/Alpha/Views/EventMap.xaml.cs
@@ -20,24 +20,24 @@ namespace Alpha.Views
 
     public sealed partial class EventMap : Page
     {
-        private Geopoint cityCenter;
 
         public EventMap()
         {
             this.InitializeComponent();
 
             // Add the MapControl and the specify maps authentication key.
-            MapControl MapControl1 = new MapControl();
-            MapControl1.ZoomInteractionMode = MapInteractionMode.GestureAndControl;
-            MapControl1.TiltInteractionMode = MapInteractionMode.GestureAndControl;
-            MapControl1.MapServiceToken = "NU84AueM7GMiPBOwHSoE~Ys6Syu5oct7d2PaDIdzl8g~AsQyIrLBonF8di9BqLCV0kg0tZnAFpbav2HztwHXPdUa3JOgL56tMLZP4E2vlTg8";
-            pageGrid.Children.Add(MapControl1);
-            MapControl1.Center = cityCenter;
+            MapControl MapControl2 = new MapControl();
+            MapControl2.ZoomInteractionMode = MapInteractionMode.GestureAndControl;
+            MapControl2.TiltInteractionMode = MapInteractionMode.GestureAndControl;
+            MapControl2.MapServiceToken = "NU84AueM7GMiPBOwHSoE~Ys6Syu5oct7d2PaDIdzl8g~AsQyIrLBonF8di9BqLCV0kg0tZnAFpbav2HztwHXPdUa3JOgL56tMLZP4E2vlTg8";
+            pageGrid.Children.Add(MapControl2);
+
+//            MapControl1.Center = cityCenter;
             MapControl1.ZoomLevel = 12;
             MapControl1.LandmarksVisible = true;
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             // Specify a known location.
             BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
@@ -47,6 +47,96 @@ namespace Alpha.Views
             MapControl1.Center = cityCenter;
             MapControl1.ZoomLevel = 12;
             MapControl1.LandmarksVisible = true;
+
+            // Set your current location.
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+
+                    // Get the current location.
+                    Geolocator geolocator = new Geolocator();
+                    Geoposition pos = await geolocator.GetGeopositionAsync();
+                    Geopoint myLocation = pos.Coordinate.Point;
+
+                    // Set the map location.
+                    MapControl1.Center = myLocation;
+                    MapControl1.ZoomLevel = 12;
+                    MapControl1.LandmarksVisible = true;
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    // Handle the case  if access to location is denied.
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+                    // Handle the case if  an unspecified error occurs.
+                    break;
+            }
+        }
+
+        private async void showStreetsideView()
+        {
+            // Check if Streetside is supported.
+            if (MapControl1.IsStreetsideSupported)
+            {
+                // Find a panorama near Avenue Gustave Eiffel.
+                BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 48.858, Longitude = 2.295 };
+                Geopoint cityCenter = new Geopoint(cityPosition);
+                StreetsidePanorama panoramaNearCity = await StreetsidePanorama.FindNearbyAsync(cityCenter);
+
+                // Set the Streetside view if a panorama exists.
+                if (panoramaNearCity != null)
+                {
+                    // Create the Streetside view.
+                    StreetsideExperience ssView = new StreetsideExperience(panoramaNearCity);
+                    ssView.OverviewMapVisible = true;
+                    MapControl1.CustomExperience = ssView;
+                }
+            }
+            else
+            {
+                // If Streetside is not supported
+                ContentDialog viewNotSupportedDialog = new ContentDialog()
+                {
+                    Title = "Streetside is not supported",
+                    Content = "\nStreetside views are not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                await viewNotSupportedDialog.ShowAsync();
+            }
+        }
+
+        private async void display3DLocation()
+        {
+            if (MapControl1.Is3DSupported)
+            {
+                // Set the aerial 3D view.
+                MapControl1.Style = MapStyle.Aerial3DWithRoads;
+
+                // Specify the location.
+                BasicGeoposition hwGeoposition = new BasicGeoposition() { Latitude = 34.134, Longitude = -118.3216 };
+                Geopoint hwPoint = new Geopoint(hwGeoposition);
+
+                // Create the map scene.
+                MapScene hwScene = MapScene.CreateFromLocationAndRadius(hwPoint,
+                                                                                     80, /* show this many meters around */
+                                                                                     0, /* looking at it to the North*/
+                                                                                     60 /* degrees pitch */);
+                // Set the 3D view with animation.
+                await MapControl1.TrySetSceneAsync(hwScene, MapAnimationKind.Bow);
+            }
+            else
+            {
+                // If 3D views are not supported, display dialog.
+                ContentDialog viewNotSupportedDialog = new ContentDialog()
+                {
+                    Title = "3D is not supported",
+                    Content = "\n3D views are not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                await viewNotSupportedDialog.ShowAsync();
+            }
         }
 
     }
