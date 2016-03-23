fd70e242316bb4fc574548d6c54b702309c72d1b
mAlxc <alexcis@webe.fr>
2016-03-23 09:57:57 +0100

    Perso

--- Dev/wua/Alpha/AlphaLib1/Contacts/Contacts.cs
+++ Dev/wua/Alpha/AlphaLib1/Contacts/Contacts.cs
@@ -8,6 +8,9 @@ namespace AlphaLib1.Contacts
 {
     public class Contacts
     {
-        
+        public string nom { get; set; }
+        public string prenom { get; set; }
+        public string mail { get; set; }
+        public string tel { get; set; }
     }
 }
---
+++ XamlCSharpApp/XamlCSharpApp/Views/Forms.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, voir la page http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace XamlCSharpApp.Views
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class Forms : Page
+    {
+        public Forms()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MyCheckBox_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
